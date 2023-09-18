fa65fc10b0f2255da1f5171ba0bb4fddeb02d291
Siddharth <siddharth05b@gmail.com>
2023-09-18 17:41:33 +0530

    encapsulation assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+      RWOnly obj = new RWOnly();
+      //obj.name = 4;
+      // since name has private access, its value cannot be directly set
+      obj.setname("Siddharth");
+      System.out.print(obj.getname());
+  }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    // getter
+    public String getname(){
+        return this.name;
+    }
+
+    // setter
+    public void setname(String name){
+        this.name = name;
+    }
 }
